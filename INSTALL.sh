2447ecad795e1b2678bc5a72ad2e062f926049ce
Rubens Carneiro <rubenscarneiro@midiaseducativas.com.br>
2018-10-19 11:47:39 -0300

    Installer setup

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
-# Install Game of Life
+echo "\x1b[32m\t- Game of Life Installer -\x1b[37m\n"
 
 INSTALL_DIR=$PWD
-
 OS="$(uname)"
 
 if [ $OS != "Linux" ]
 then
-    echo "Not running on Linux. Current OS: ($OS) \n"
+    echo "\x1b[31mNot running on Linux. Current OS: ($OS) \x1b[37m\n"
     exit 1
 fi
 
+# App files 
 mkdir --parents /opt/drakmord/life/assets/images
-
-cp $INSTALL_DIR/RUN /opt/drakmord/life
+cp $INSTALL_DIR/bin/RUN /opt/drakmord/life
 cp $INSTALL_DIR/assets/images/icon.png /opt/drakmord/life/assets/images
+echo "$INSTALL_DIR" > /opt/drakmord/life/config.cfg
 
 # Create desktop shortcut
 cp $INSTALL_DIR/Life.desktop /usr/share/applications
+
+echo "\x1b[32m- Installation sucessful\x1b[37m\n"
